f0888207989b155951a68120749ded7627f4827d
Jherson1017 <jhersonalaya10@gmail.com>
2026-05-05 10:55:11 -0500

    Colecciones

---
+++ src/programacolecciones/ProgramaColecciones.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programacolecciones;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DELL
+ */
+public class ProgramaColecciones {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Persona p1 = new Persona("40703678","Edu");
+        Persona p2= new Persona("45219990","Lucy");
+        ArrayList<Persona> lista1 = new ArrayList <Persona>();
+        lista1.add(p1);
+        lista1.add(p2);
+        System.out.println(Clista1);
+    }  
+}
